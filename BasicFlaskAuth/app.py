66294c820b766138042541712802952cfb3f3050
Stefano Bettinelli <ste.bettinelli@gmail.com>
2020-08-03 20:39:44 +0200

    Authorization exercise

--- BasicFlaskAuth/app.py
+++ BasicFlaskAuth/app.py
@@ -120,21 +120,44 @@ def verify_decode_jwt(token):
     )
 
 
-def requires_auth(f):
-    @wraps(f)
-    def wrapper(*args, **kwargs):
-        token = get_token_auth_header()
-        try:
-            payload = verify_decode_jwt(token)
-        except:
-            abort(401)
-        return f(payload, *args, **kwargs)
+def check_permissions(permission, payload):
+    if "permissions" not in payload:
+        raise AuthError(
+            {
+                "code": "invalid_claims",
+                "description": "Permissions not included in JWT.",
+            },
+            400,
+        )
 
-    return wrapper
+    if permission not in payload["permissions"]:
+        raise AuthError(
+            {"code": "unauthorized", "description": "Permission not found."}, 403
+        )
+    return True
+
+
+def requires_auth(permission=""):
+    def requires_auth_decorator(f):
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            token = get_token_auth_header()
+            try:
+                payload = verify_decode_jwt(token)
+            except:
+                abort(401)
+
+            check_permissions(permission, payload)
+
+            return f(payload, *args, **kwargs)
+
+        return wrapper
+
+    return requires_auth_decorator
 
 
 @app.route("/headers")
-@requires_auth
+@requires_auth("get:images")
 def headers(payload):
     print(payload)
     return "Access Granted"
