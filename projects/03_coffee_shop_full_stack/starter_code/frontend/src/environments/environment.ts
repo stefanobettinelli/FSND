ba16946457e5b60c0335ae305071783214a80983
Stefano Bettinelli <ste.bettinelli@gmail.com>
2020-08-06 19:22:41 +0200

    Solution Coffee shop

--- projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
+    url: 'fsnd-stef.eu', // the auth0 domain prefix
+    audience: 'fsnd-coffee-shop-api', // the audience set for the auth0 app
+    clientId: 'iN7VWkrFlkykT40RSZy6sSs4cJPsfZ9V', // the client id generated for the auth0 app
+    callbackURL: 'http://127.0.0.1:8100', // the base url of the running ionic application.
   }
 };
